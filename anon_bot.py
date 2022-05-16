9338a65c3f092fd77227f4b3b8387801d644f018
t3m14 <artemon.m174@gmail.com>
2022-05-16 16:14:15 +0300

    d

--- anon_bot.py
+++ anon_bot.py
@@ -16,7 +16,6 @@ WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-# webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.getenv('PORT'))
 token = BOT_TOKEN
