90d93038760a10f8c9608f62343784de413b0794
Артём <78920456+t3m14@users.noreply.github.com>
2022-06-05 08:08:35 +0300

    Update anon_bot.py

--- anon_bot.py
+++ anon_bot.py
@@ -10,14 +10,6 @@ if not BOT_TOKEN:
     print('You have forgot to set BOT_TOKEN')
     quit()
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = int(os.getenv('PORT'))
 token = BOT_TOKEN
 admin_id = "2132310485"
 storage = MemoryStorage()
